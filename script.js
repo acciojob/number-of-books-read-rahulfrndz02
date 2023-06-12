d693a45a356829d076f878ebd2f0801a4dd9d946
Rahul Kumar Pandit <55670332+rahulfrndz02@users.noreply.github.com>
2023-06-12 17:31:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ const numberOfBooksRead = () => {
 	let count=0;
 	for(let i=0; i<library.length; i++){
 		library[i].readingStatus && count++;
+		if(library[i].readingStatus === true)
 		
 	}
 	return count;
